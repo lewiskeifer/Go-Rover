84b5eec62c3bd03d299bb4a01cfa206da6787651
unknown <lewiskeifer@yahoo.com>
2025-12-03 17:15:18 -0500

    update move and print methods

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type Lateral int
 
 const (
 	LEFT  Lateral = iota // 0
-	RIGHT                // 1 	// 3
+	RIGHT                // 1
 )
 
 type Direction int
@@ -27,12 +27,27 @@ type Rover struct {
 	x         int
 	y         int
 	direction Direction
-	actions   []int
+	path      [][2]int // track all positions visited
 }
 
-func (r *Rover) Move(dx, dy int) {
-	r.x += dx
-	r.y += dy
+func (r *Rover) Move(distance int) {
+	for step := 0; step < distance; step++ {
+		switch r.direction {
+		case NORTH:
+			r.y++
+		case EAST:
+			r.x++
+		case SOUTH:
+			if r.y > 0 {
+				r.y--
+			}
+		case WEST:
+			if r.x > 0 {
+				r.x--
+			}
+		}
+		r.path = append(r.path, [2]int{r.x, r.y})
+	}
 }
 
 func (r *Rover) Rotate(newDirection Lateral) {
@@ -45,17 +60,55 @@ func (r *Rover) Rotate(newDirection Lateral) {
 }
 
 func (r *Rover) Print() {
-	for i := 0; i < r.x; i++ {
-		for j := 0; j < r.y; j++ {
-			fmt.Print("-")
+	if len(r.path) == 0 {
+		fmt.Println("No path yet")
+		return
+	}
+
+	minX, maxX := r.path[0][0], r.path[0][0]
+	minY, maxY := r.path[0][1], r.path[0][1]
+	for _, p := range r.path {
+		if p[0] < minX {
+			minX = p[0]
 		}
-		fmt.Println("x")
+		if p[0] > maxX {
+			maxX = p[0]
+		}
+		if p[1] < minY {
+			minY = p[1]
+		}
+		if p[1] > maxY {
+			maxY = p[1]
+		}
+	}
+
+	// Build grid
+	width := maxX - minX + 1
+	height := maxY - minY + 1
+	grid := make([][]rune, height)
+	for i := range grid {
+		grid[i] = make([]rune, width)
+		for j := range grid[i] {
+			grid[i][j] = '.' // empty space
+		}
+	}
+
+	// Mark path
+	for _, p := range r.path {
+		grid[maxY-p[1]][p[0]-minX] = '*' // invert Y for printing
+	}
+
+	// Mark rover
+	grid[maxY-r.y][r.x-minX] = 'R'
+
+	// Print grid
+	for _, row := range grid {
+		fmt.Println(string(row))
 	}
 }
 
 func main() {
-
-	rover := Rover{}
+	rover := Rover{path: [][2]int{{0, 0}}}
 	name := ""
 	reader := bufio.NewReader(os.Stdin)
 
@@ -70,7 +123,7 @@ func main() {
 
 		switch name {
 		case "m":
-			rover.Move(1, 1)
+			rover.Move(1)
 		case "r":
 			rover.Rotate(RIGHT)
 		case "p":
@@ -79,5 +132,5 @@ func main() {
 	}
 
 	fmt.Println("Rover is at: ", rover.x, rover.y)
-	fmt.Println("Rover is facing: ", rover.direction)
+	fmt.Println("Rover is facing: ", Direction(rover.direction))
 }
